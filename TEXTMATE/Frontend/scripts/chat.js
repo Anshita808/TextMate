3dced8f3967f37762d7636a1e989aae43255d852
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-05-05 20:43:39 +0530

    3rd-commit-day3

--- TEXTMATE/Frontend/scripts/chat.js
+++ TEXTMATE/Frontend/scripts/chat.js
@@ -7,8 +7,54 @@ const socket = io("http://localhost:8080/",{transports:["websocket"]});
     const typing = document.querySelector("h4");
     const lastChange = document.querySelector("h5")
 
-    const username =prompt('What is your name?');
-    micOff()
+      
+    const join = document.getElementById('join');
+    const create = document.getElementById('create');
+    var username = "Anonymous"
+    join.addEventListener('click',()=>{
+        document.getElementById("join-form").style.display="block"
+        document.getElementById("create-form").style.display="none"
+
+        join.classList.add("curr")
+        create.classList.remove("curr")
+    })
+    create.addEventListener('click',()=>{
+        document.getElementById("join-form").style.display="none"
+        document.getElementById("create-form").style.display="block"
+
+        create.classList.add("curr")
+        join.classList.remove("curr")
+    })
+
+    const join_btn = document.getElementById('join-btn');
+    const create_btn = document.getElementById('create-btn');
+
+    join_btn.addEventListener('click',(e)=>{
+        e.preventDefault()
+
+        document.getElementById("mainContaint").style.display="block"
+        document.getElementById("form").style.display="none"
+
+        var room = document.querySelector("#join-form>input[type=text]").value
+        console.log(room)
+
+        socket.emit('joinRoom', {username,room});
+    })
+
+    create_btn.addEventListener('click',(e)=>{
+        e.preventDefault()
+
+        document.getElementById("mainContaint").style.display="block"
+        document.getElementById("form").style.display="none"
+
+        var room = document.querySelector("#create-form>input[type=text]").value
+        console.log(room)
+
+        socket.emit('joinRoom', {username,room});
+    })
+
+    //prompt('What is your name?') ||
+    
     text.addEventListener("keyup",(e)=>{
         socket.emit('typing', username);
         setTimeout(() => {
@@ -24,23 +70,19 @@ const socket = io("http://localhost:8080/",{transports:["websocket"]});
         typing.innerHTML=``;
     })
 
-    socket.on('connect', () => {
-        const room ="dsa501"
-        socket.emit('joinRoom', {username,room});
-    });
+    
 
     socket.on("message",(message)=>{
-        typewriter(message);
+        typewriter(message)
     })
 
     function typewriter(message){
-        var typewriterText = ""
-        typewriterText = message
-        joinAlert.style.visibility="visible"
+        var typewriterText = message
 
         const typewriterDelay = 100;
         const typewriterElement = document.getElementById("typewriter");
         typewriterElement.innerHTML = ""
+        document.getElementById("alert").style.visibility="visible"
         let i = 0;
         typeWriter();
         function typeWriter() {
@@ -52,7 +94,7 @@ const socket = io("http://localhost:8080/",{transports:["websocket"]});
         }
         setTimeout(function(){
             document.getElementById("alert").style.visibility="hidden"
-        }, 3000)
+        }, 10000)
     }
 
     socket.on("roomUsers",({room,users})=>{
@@ -67,28 +109,61 @@ const socket = io("http://localhost:8080/",{transports:["websocket"]});
         users.forEach(user => {
             let firstLetter = user.username.toUpperCase()[0]
             html+=`<div>
-                        <h1>${firstLetter}</h1>
+                        <h1 id="${user.username}">${firstLetter}</h1>
                         <p>${user.username}</p>
                     </div>`
         });
         userList.innerHTML = html
     }
 
+    var localStream;
+    var remoteStream;
 
     function micOff(){
-        console.log("micOff")
         document.getElementById("micOff").style.display="block"
         document.getElementById("micOn").style.display="none"
+
+        localStream.getTracks().forEach(track => {
+            track.stop();
+        });
+        const status = false
+        socket.emit('stream', { room,status });
     }
     function micOn(){
-        console.log("micOn")
         document.getElementById("micOff").style.display="none"
         document.getElementById("micOn").style.display="block"
+
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+                localStream = stream;
+                document.getElementById("localAudio").srcObject = stream;
+
+                const status = true
+                socket.emit('stream', { room,status });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
+    socket.on("stream",({user,status})=>{
+        if(status){
+            document.getElementById(`${user.username}`).style.border = "5px solid #61f83c";
+        }else{
+            document.getElementById(`${user.username}`).style.border = "2px solid #0efd8e";
+        }
+    })
 
     document.querySelector("#allChats~input").addEventListener("change", ()=>{
-        socket.emit('chat', document.querySelector("#allChats~input").value);
-        document.querySelector("#allChats~input").value=""
+        if(document.querySelector("#allChats~input").value!=""){
+            socket.emit('chat', document.querySelector("#allChats~input").value);
+            document.querySelector("#allChats~input").value=""
+        }
+    })
+    document.querySelector("#allChats~input~ion-icon").addEventListener("click", ()=>{
+        if(document.querySelector("#allChats~input").value!=""){
+            socket.emit('chat', document.querySelector("#allChats~input").value);
+            document.querySelector("#allChats~input").value=""
+        }
     })
     socket.on("chat",(chat)=>{
         appendChat(chat)
@@ -105,4 +180,19 @@ const socket = io("http://localhost:8080/",{transports:["websocket"]});
         const allChats = document.getElementById('allChats');
         allChats.scrollTop = allChats.scrollHeight;
     }
+    
+    function updateClock() {
+        const now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        let amPm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        const timeString = hours + ':' + minutes + ' ' + amPm;
+        const dateString = now.toLocaleString('default', { month: 'long', day: 'numeric' });
+        document.getElementById('clock').innerHTML = timeString +" "+"-" +" "+ dateString;
+      }
+      
+      setInterval(updateClock,1000);
+      
       
